1391079b28eaccf7fec60b132b13f9fc4bceff39
ido wajnbuch <idoyosefw@gmail.com>
2025-06-24 12:21:54 +0300

    added basic popup window

--- my-app/src/Comment/Comment.tsx
+++ my-app/src/Comment/Comment.tsx
@@ -2,8 +2,26 @@ import React from 'react';
 import { FaComment } from 'react-icons/fa';
 
 const Comment : React.FC = () => {
-    return (<button className='comment' style={{background:'none', border:'none'}}>
-        <FaComment color="gray"></FaComment></button>);
+    const [openPopup, setOpenPopup] = React.useState(false);
+
+    return(
+        <span className='comment-wrapper'>
+            <button className='comment' style={{background:'none', border:'none'}}
+                onClick={() => setOpenPopup(true)}>
+                <FaComment color="gray"></FaComment>
+            </button>
+            {
+                openPopup &&
+                <div className='comments-popup'>
+                    <div className='comments-popup-inner'>
+                        <button className='close-popup' onClick={() => setOpenPopup(false)}>X</button>
+                        ido
+                    </div>
+                </div>
+            }
+
+        </span>
+    );
 }
 
 export default Comment;
