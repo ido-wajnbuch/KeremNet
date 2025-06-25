773613144cf2c5382b4f557d0b716e80e72ecd10
ido wajnbuch <idoyosefw@gmail.com>
2025-06-25 18:30:54 +0300

    added basic not found page

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App />,
+    errorElement: <div>hi! you reached to page that not found</div>
+  }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
 
